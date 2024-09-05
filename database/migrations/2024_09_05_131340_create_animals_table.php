7b7067aaf350986b6f712c9a0695ecc535e7ddc1
MandySpaan <mandyspaan94@gmail.com>
2024-09-05 16:29:33 +0200

    feat(migrations): added more columns to animals

--- database/migrations/2024_09_05_131340_create_animals_table.php
+++ database/migrations/2024_09_05_131340_create_animals_table.php
@@ -1,31 +1,42 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
-    {
-        Schema::create('animals', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('scientific_name')->nullable();
-            $table->text('description')->nullable();
-            $table->string('image_url')->nullable();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up()
+   {
+       Schema::create('animals', function (Blueprint $table) {
+           $table->id();
+           $table->string('name');
+           $table->string('scientific_name')->nullable();
+           $table->text('description')->nullable();
+           $table->string('image_url')->nullable();
+           $table->enum('category', ['vertebrate', 'invertebrate']);
+           $table->enum('type', ['mammal', 'bird', 'reptile', 'amphibian', 'fish',
+               'Cnidaria', 'Arthropoda', 'Mollusca', 'Annelida', 'Echinodermata']);
+           $table->enum('diet', ['Herbivore', 'Carnivore', 'Omnivore']);
+           $table->enum('habitat', ['Terrestrial', 'Aquatic', 'Arboreal']);
+           $table->timestamps();
+       });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('animals');
-    }
+
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+       Schema::dropIfExists('animals');
+   }
 };
+
+
+
